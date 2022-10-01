09712617af45cba8178a9519f1c98f36a55e3900
Aaron Lio <aaronlio003@gmail.com>
2022-10-01 02:03:41 -0400

    Lab 2 Part 2

--- Lab_2/part2.sv
+++ Lab_2/part2.sv
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ns
+
 module v7404 (pin1, pin3, pin5, pin9, pin11, pin13, pin2, pin4, pin6, pin8, pin10, pin12);
 
     input logic pin1;
@@ -79,26 +81,26 @@ module mux2to1(x, y, s, m);
     logic y_s;
 
     v7404 nots(
-        .pin1[s],
-        .pin2[not_s]
+        .pin1(s),
+        .pin2(not_s)
         );
 
     v7408 xandnots(
-        .pin1[x],
-        .pin2[not_s],
-        .pin3[x_ns]
+        .pin1(x),
+        .pin2(not_s),
+        .pin3(x_ns)
         );
 
     v7408 yands(
-        .pin13[y],
-        .pin12[s],
-        .pin11[y_s]
+        .pin13(y),
+        .pin12(s),
+        .pin11(y_s)
         );
     
     v7432 y_s_or_x_ns(
-        .pin1[x_ns],
-        .pin2[y_s],
-        .pin3[m])
+        .pin1(x_ns),
+        .pin2(y_s),
+        .pin3(m))
     
 
 endmodule  
