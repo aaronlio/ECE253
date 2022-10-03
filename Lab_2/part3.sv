551d0f76f0644e5bad5b0945caad291eb219d757
Aaron Lio <lioaaron@WIN.ecf.utoronto.ca>
2022-10-03 14:54:59 -0400

    Lab 2 part 3

--- Lab_2/part3.sv
+++ Lab_2/part3.sv
@@ -1,8 +1,17 @@
-module hex_decoder(c, display);
-        input logic [3:0] c;
-        output logic [6:0] display;
 
-        assign display[0] = ~c[3] & ~c[2] & ~c[1] & c[0] | ~c[3] & c[2] & ~c[1] & ~c[0] | c[3] & ~c[2] & c[1] & c[0]| c[3] & c[2] & ~c[1] & c[0];
+module hex(SW, HEX0);
+	input logic [9:0] SW;
+	output logic [6:0] HEX0;
+	
+	hex_decoder u0(.c(SW[3:0]), .display(HEX0[6:0]));
+	
+endmodule
+
+module hex_decoder(c, display);
+   input logic [3:0] c;
+   output logic [6:0] display;
+
+   assign display[0] = ~c[3] & ~c[2] & ~c[1] & c[0] | ~c[3] & c[2] & ~c[1] & ~c[0] | c[3] & ~c[2] & c[1] & c[0]| c[3] & c[2] & ~c[1] & c[0];
 	assign display[1] = ~c[3] & c[2] & ~c[1] & c[0] | ~c[3] & c[2] & c[1] & ~c[0] | c[3] & ~c[2] & c[1] & c[0]| c[3] & c[2] & ~c[1] & ~c[0] | c[3] & c[2] & c[1] & ~c[0] | c[3] & c[2] & c[1] & c[0];
 	assign display[2] = ~c[3] & ~c[2] & c[1] & ~c[0] | c[3] & c[2] & ~c[1] & ~c[0] | c[3] & c[2] & c[1] & ~c[0]| c[3] & c[2] & c[1] & c[0];
 	assign display[3] = ~c[3] & ~c[2] & ~c[1] & c[0] | ~c[3] & c[2] & ~c[1] & ~c[0] | ~c[3] & c[2] & c[1] & c[0]| c[3] & ~c[2] & c[1] & ~c[0] | c[3] & c[2] & c[1] & c[0];
