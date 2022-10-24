b0795b309626521f5b2ddfc0e4caa293d91400a8
Aaron Lio <aaronlio003@gmail.com>
2022-10-24 13:59:47 -0400

    Lab 4 ad.

--- Lab_4/part3.sv
+++ Lab_4/part3.sv
@@ -1,26 +1,20 @@
 module part3(clock, reset, ParallelLoadn, RotateRight, ASRight, Data_IN, Q);
     input logic clock, reset, ParallelLoadn, RotateRight, ASRight;
-    input [3:0] Data_IN;
+    input logic [3:0] Data_IN;
     output logic [3:0] Q;
 
     logic temp;
-
-    always_comb 
-    begin
-        case(RotateRight & ASRight)
-        0: temp = Q[0];
-        1: temp = Q[3]; 
-        endcase
-    end
+    
+    mux2to1 m0(.x(Q[0]), .y(Q[3]), .s(ASRight), .f(temp));
 
     rotate r0(.clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .left(Q[1]), .right(Q[3]), .D(Data_IN[0]), .Q(Q[0]));
     rotate r1(.clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .left(Q[2]), .right(Q[0]), .D(Data_IN[1]), .Q(Q[1]));
     rotate r2(.clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .left(Q[3]), .right(Q[1]), .D(Data_IN[2]), .Q(Q[2]));
-    rotate r3(.clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .left(Q[temp]), .right(Q[2]), .D(Data_IN[3]), .Q(Q[3]));
+    rotate r3(.clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .left(temp), .right(Q[2]), .D(Data_IN[3]), .Q(Q[3]));
 endmodule
 
 module rotate(clock, reset, loadn, LoadLeft, left, right, D, Q);
-    input logic clock, reset, ParallelLoadn, RotateRight, left, right, D;
+    input logic clock, reset, loadn, LoadLeft, left, right, D;
     output logic Q;
     
     logic F, R;
@@ -31,7 +25,7 @@ module rotate(clock, reset, loadn, LoadLeft, left, right, D, Q);
 endmodule
 
 module mux2to1(x, y, s, f);
-    input logic x, y, x;
+    input logic x, y, s;
     output logic f;
 
     always_comb 
