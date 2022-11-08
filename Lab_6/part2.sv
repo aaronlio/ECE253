11becc2c3b5102b27f1a36fb0b939bf5f83702c3
Aaron Lio <aaronlio003@gmail.com>
2022-11-08 15:04:22 -0500

    Lab 6

--- Lab_6/part2.sv
+++ Lab_6/part2.sv
@@ -172,7 +172,7 @@ module control(
             end
             S_CYCLE_4: begin // r <- Ax*x+ Bx + C
                 ld_r = 1'b1;
-                alu_select_b = 2'b10;
+                alu_select_b = 2'b01;
             end
         // We don't need a default case since we already made sure all of our outputs were assigned a value at the start of the always block.
         endcase
