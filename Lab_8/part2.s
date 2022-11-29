2f2adc673e1b86d463d647ec68aa9510377e9767
Aaron Lio <lioaaron@WIN.ecf.utoronto.ca>
2022-11-28 19:17:09 -0500

    8

--- Lab_8/part2.s
+++ Lab_8/part2.s
@@ -1,16 +1,14 @@
 # Vars
-	# s2: holds entire list (s8)
-	# s1: holds number of items in list (s7)
-	# t1: outer loop index (s1)
-	# t2: inner loop index (s2)
+	# s2: holds entire list
+	# s1: holds number of items in list
+	# t1: outer loop index
+	# t2: inner loop index
 	# t3: 1
 
-	# t5: holds next list index
-	# t6: holds list shifted to next word
-	# a1: holds list (current item at index zero)
-	# a2: holds shifted list
+	# t5: holds value to compare #1
+	# t6: holds value to compare #2
+	# a1: holds shifted list (current item at index zero)
 	
-
 .global _start
 .text
 _start:
@@ -19,7 +17,6 @@ _start:
 	lw s1, 0(s2)
 	addi t1, s1, 1 #outer loop index
 	addi t2, zero, 1 # inner loop index
-	
 	
 	addi t3, zero, 1 # register to hold constant value 1
 
@@ -33,18 +30,16 @@ LOOP:
 	beq t1, t2, LOOP_size 
 	
 	slli a1, t2, 2 # j * 4 to get next elem address in list
-	add a1, a1, s2
-	
-	#addi a2, a1, 4
+	add a1, a1, s2 # shift list to so value to compare is at first index
 	
 	jal SWAP
-	add t2, t2, t3
+	add t2, t2, t3 # increment j
 	j LOOP
 
 SWAP: 
 	lw t5, 0(a1)
-	#lw t6, 0(a2)
 	lw t6, 4(a1)
+	
 	bgt t6, t5, CORRECT_ORDER
 	
 	sw t6, 0(a1)
